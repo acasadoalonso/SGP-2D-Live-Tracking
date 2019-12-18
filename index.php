406e5a0a35be4dbb8ce1b0429e2246a61723ae04
Angel Casado <acasadoalonso@gmail.com>
2019-12-18 18:32:48 +0100

    drop support for mobile

--- index.php
+++ index.php
@@ -3,13 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'node/mobile.detect.php';
-$detect = new Mobile_Detect;
 require_once 'node/config.php';
-$deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
 
-if($deviceType=="phone") redirect($AppUrl."/node/ogn.phone.php");
-if($deviceType!="phone") redirect($AppUrl."/node/ogn.desktop.php");
+redirect($AppUrl."/node/ogn.desktop.php");
 
 function redirect($url, $statusCode = 303)
 {
