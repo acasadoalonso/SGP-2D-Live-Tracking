925b937cdeebb1aecee9c7301b90524b369ec0c8
Angel Casado <acasadoalonso@gmail.com>
2020-07-14 13:09:05 +0200

    add support for ID & REG on lastfix.php

--- lastfix.php
+++ lastfix.php
@@ -5,12 +5,38 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set("UTC");
 include ("node/config.php");
-
-	
-$query="SELECT  * FROM `GLIDERS_POSITIONS` ORDER BY lastFixTx DESC ;";
+if (isset($_GET['ID']))
+        {
+        $ID = $_GET['ID'];
+        $query="SELECT  * FROM `GLIDERS_POSITIONS` where flarmId = '".$ID."' ORDER BY lastFixTx DESC ;";
+        }
+elseif (isset($_GET['REG']))
+        {
+        $REG = $_GET['REG'];
+        $query1="SELECT  idglider FROM `GLIDERS` where registration = '".$REG."' ;";
+        }
+else
+        {
+        $query="SELECT  * FROM `GLIDERS_POSITIONS` ORDER BY lastFixTx DESC ;";
+        }
 $db = new mysqli($servername, $DBuser, $DBpasswd, $dbname);
 
-$results = $db->query($query);
+if (isset($_GET['REG']))
+        {
+        $results = $db->query($query1);
+        if ($results->num_rows > 0) 
+            {
+            $row=$results->fetch_array() ;
+            //echo $row[0];
+            $ID=$row[0];
+            $query2="SELECT  * FROM `GLIDERS_POSITIONS` where substr(flarmId,4) = '".$ID."' ORDER BY lastFixTx DESC ;";
+            $results = $db->query($query2);
+            }
+        }
+else
+        {
+        $results = $db->query($query);
+        }
 $last[0]="";
 $i=0;
 if ($results->num_rows > 0) 
