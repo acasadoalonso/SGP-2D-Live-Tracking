027436907cf36727a5e8c991613492675095f40a
Angel Casado <acasadoalonso@gmail.com>
2020-08-15 19:24:57 +0200

    adjust trkstatus

--- trkstatus.py
+++ trkstatus.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python
-#
-# Silent Wings interface --- JSON formaat
-#
-import json
-import time
 import sys
 import os
 import MySQLdb
@@ -16,7 +10,7 @@ import config
 #   This script set the pairing between OGN trackers and flarms that are on the same glider, so it become a virtual single device
 #
 
-action='list'
+action='list'								# set the defaults
 trk='ALL'
 #print (sys.argv)
 if len(sys.argv) >1:							# first arg is the action
@@ -33,12 +27,13 @@ DBpath   = config.DBpath						# use the configuration DB path
 DBname   = config.DBname						# use the configuration DB name
 DBname   ='APRSLOG'							# database always APRSLOG
 DBtable  ='TRKSTATUS'							# table name
+if os.path.exists("./img/ogn-logo-150x150.png"):
+   html1 = """<head><meta charset="UTF-8"><meta http-equiv="refresh" content="30" > </head><TITLE>TRACKERs Status</TITLE> <IMG src="./img/ogn-logo-150x150.png" border=1 alt=[image]><H1>WGC CAT I Trackers STATUS: </H1> <HR> <P>Today is:  %s and we have %d trackers on TRKDEVICES table.  <br /> <br /> <br /> </p> </HR> """
+else:
+   html1 = """<head><meta charset="UTF-8"><meta http-equiv="refresh" content="30" > </head><TITLE>TRACKERs Status</TITLE> <IMG src="./node/img/ogn-logo-150x150.png" border=1 alt=[image]><H1>WGC CAT I Trackers STATUS: </H1> <HR> <P>Today is:  %s and we have %d trackers on TRKDEVICES table.  <br /> <br /> <br /> </p> </HR> """
 
-html1 = """<head><meta charset="UTF-8"><meta http-equiv="refresh" content="30" > </head><TITLE>TRACKERs Status</TITLE> <IMG src="./img/ogn-logo-150x150.png" border=1 alt=[image]><H1>WGC CAT I Trackers STATUS: </H1> <HR> <P>Today is:  %s and we have %d trackers on TRKDEVICES table.  <br /> <br /> <br /> </p> </HR> """
 html2 = """<center><table><tr><td><pre>"""
 html3 = """</pre></td></tr></table></center>"""
-html4 = '<a href='+"UBUNTU"+'SWS/pairtrk.php?action=edit&trk=%s&flarmid=%s&owner=%s&active=%s'
-
 
 #
 conn = MySQLdb.connect(host=config.DBhost, user=config.DBuser, passwd=config.DBpasswd, db=DBname, connect_timeout=1000)     # connect with the database
@@ -47,43 +42,68 @@ curs = conn.cursor()				# connect with the DB set the cursor
 #
 # get the counter of trackers pairs active
 #
-cmd1 = "select count(*) from TRKDEVICES where devicetype = 'OGNT' and active = 1;"
+sql1 = "SELECT COUNT(*) FROM TRKDEVICES WHERE devicetype = 'OGNT' AND active = 1;"
 try:
-        curs.execute(cmd1)
+        curs.execute(sql1)
 except MySQLdb.Error as e:
         print ("SQL error: ",e)
 row = curs.fetchone()
 nrecs=row[0]					# number of trackers pairs active
-cmd0="SELECT   count(*) FROM OGNTRKSTATUS ;"
+
+sql0="SELECT   COUNT(DISTINCT(id)) FROM OGNTRKSTATUS ;"
+cmd0="SELECT   COUNT(*) FROM OGNTRKSTATUS ;"
 cmd1="SELECT * FROM OGNTRKSTATUS where source = 'STAT' and id in (select id from TRKDEVICES where active = 1 and devicetype = 'OGNT') ORDER BY otime DESC;"
 cmd2="SELECT * FROM OGNTRKSTATUS where source = 'STAT' and id = '"+trk+"' ORDER BY otime DESC;"
 cmd3="SELECT * FROM OGNTRKSTATUS ORDER BY otime DESC;"
+cmd4="SELECT DISTINCT (id) FROM `OGNTRKSTATUS`;" 
 try:
-        curs.execute(cmd0)
+        curs.execute(sql0)			# get the number of trackers seen
+except MySQLdb.Error as e:
+        print ("SQL error: ",e)
+cnt=curs.fetchone()
+try:
+        curs.execute(cmd0)			# get the number of records on the TRK STAUS table
 except MySQLdb.Error as e:
         print ("SQL error: ",e)
 row=curs.fetchone()
-print ("Number of records on TRKSTATUS table:", row[0])	# number of records on the table
+print ("Number of records on TRKSTATUS table:", row[0], " as today: ", today , " from ", cnt[0], " trackers </br>")	# number of records on the table
+
 if trk == 'ALL':
         if action == 'full':
            cmd = cmd3				# if all the trackers
+        elif action == 'lastfix':
+           cmd = cmd4				# if all the trackers
         else:
            cmd = cmd1				# if only the active trackers
 else:
 	cmd = cmd2 				# if looking for an specific TRACK ID
 #print (cmd)
 try:
-        curs.execute(cmd)
+        curs.execute(cmd)			# get the info from OGNTRKSTATUS table
 except MySQLdb.Error as e:
         print ("SQL error: ",e)
 trks={}						# the list of trackers and its counters
-trkt={}						# teh list of trackers and the last OTIME
-print (html1% (today,nrecs))
+trkt={}						# the list of trackers and the last OTIME
+print (html1% (today,nrecs))			# report data and number of pairs
 print (html2)
-print ("<b> <a>TRKDEV  IDTRK     REGTRK CID STATION       UTCTIME            STATUS     r </a> </b> <br />") 
+print ("<b> <a>TRKDEV  IDTRK     REGTRK CID STATION       UTCTIME            STATUS     SOURCE </a> </b> <br />") 
 for row in curs.fetchall():                     # search 
         # flarmid is the first field
         id1      = row[0]			# tracker ID
+        if action == 'lastfix':
+           cmd5="SELECT MAX(otime), id FROM OGNTRKSTATUS WHERE id = '"+id1+"' ;"
+           try:					# find the last fix
+              curs.execute(cmd5)		# get the info from OGNTRKSTATUS table
+           except MySQLdb.Error as e:
+              print ("SQL error: ",e)
+           row=curs.fetchone()
+           mtime=row[0]
+           maxotime = mtime.strftime("%y-%m-%d %H:%M:%S")			# in string format yymmdd
+           cmd6="SELECT * FROM OGNTRKSTATUS WHERE id = '"+id1+"' and otime = '"+maxotime+"' ;"
+           curs.execute(cmd6)			# get the info from OGNTRKSTATUS table
+           row=curs.fetchone()
+        if row == None:
+              continue
         station  = row[1]			# station receiving it
         otime    = row[2]			# timestamp
         status   = row[3]			# status
@@ -94,7 +114,7 @@ for row in curs.fetchall():                     # search
         if sp == 20:				# if a encrypted status, nothing to do
            continue
         if id1 not in trks:			# if not seeing yet
-           trks[id1] = 1
+           trks[id1] = 1			# add it now
         else:
            trks[id1] += 1			# increase the counter
         if status[6:8] == 'h ':			# check the version number
@@ -111,7 +131,7 @@ for row in curs.fetchall():                     # search
            continue 
         print ("<a> TRKDEV: %-9s %-7s %-3s %-9s %-20s %-30s %4s "% (id1, reg, cid, station, str(otime), status, source), "</a>")
 
-print (html3)
+print (html3)					# print the end of HTML table
 conn.close()
 
 exit(0)
